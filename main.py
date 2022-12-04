f1aab271de5ec3ebc7bf0fc1123c06efd45b68ee
George Khananaev <george.khananaev@gmail.com>
2022-12-04 14:53:14 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+#
+# 8888888b.                  888b     d888               888 d8b                  .d8888b.
+# 888   Y88b                 8888b   d8888               888 Y8P                 d88P  Y88b
+# 888    888                 88888b.d88888               888                     888    888
+# 888   d88P 888  888        888Y88888P888  .d88b.   .d88888 888  8888b.         888         .d88b.  88888b.d88b.  88888b.  888d888 .d88b.  .d8888b  .d8888b   .d88b.  888d888
+# 8888888P"  888  888        888 Y888P 888 d8P  Y8b d88" 888 888     "88b        888        d88""88b 888 "888 "88b 888 "88b 888P"  d8P  Y8b 88K      88K      d88""88b 888P"
+# 888        888  888        888  Y8P  888 88888888 888  888 888 .d888888        888    888 888  888 888  888  888 888  888 888    88888888 "Y8888b. "Y8888b. 888  888 888
+# 888        Y88b 888        888   "   888 Y8b.     Y88b 888 888 888  888        Y88b  d88P Y88..88P 888  888  888 888 d88P 888    Y8b.          X88      X88 Y88..88P 888
+# 888         "Y88888        888       888  "Y8888   "Y88888 888 "Y888888         "Y8888P"   "Y88P"  888  888  888 88888P"  888     "Y8888   88888P'  88888P'  "Y88P"  888
+#                 888                                                                                              888
+#            Y8b d88P                                                                                              888
+#             "Y88P"                                                                                               888                                                                           /____/
+#
+# github: https://github.com/georgekhananaev/py-image-compressor
+
 from components import imageCompressor as iC, mainFunctions as mF, localColors as Color
 import concurrent.futures
 import argparse
