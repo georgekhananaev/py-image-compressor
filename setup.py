b07b1e36a993f7c114e26ee2ed1bb72875ec5c5f
George Khananaev <106206490+georgekhananaev@users.noreply.github.com>
2025-01-26 20:09:07 +0700

    added pypi workflow

---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="py-image-compressor",
+    version="1.0.0",
+    description="A Python bulk image compressor and resizer",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="George Khananaev",
+    author_email="george.khananaev@gmail.com",
+    url="https://github.com/georgekhananaev/py-image-compressor",
+    packages=find_packages(),
+    install_requires=[
+        "Pillow>=11.1.0",
+        "tqdm>=4.67.1",
+        "pillow-heif>=0.21.0",
+        "pillow-avif-plugin>=1.4.6",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
